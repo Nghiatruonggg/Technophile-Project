1eb042a54289611ac64d6da6532370d433ad4da7
Nghiatruonggg <154891329+Nghiatruonggg@users.noreply.github.com>
2024-06-26 22:04:28 +0700

    Update 26.6

---
+++ assets/js/product-detail.js
@@ -0,0 +1,37 @@
+// Product Image Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: -35,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+// Viewed Product OwlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
